2872654cbc0db3805cfbbf3fba27c88c6dd3bdc8
APPLE <>
2018-10-14 15:04:45 +0900

    ##77. 중첩된 라우트

--- react/section6/simpleStart/src/routes.js
+++ react/section6/simpleStart/src/routes.js
@@ -3,6 +3,24 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
+// ** 일반적으로 다른 라우터 파일을 이곳에 정의 하지 않는다.
+const Greeting = ()=>{
+    return <div>hey there!</div>;
+}
+
 export default (
-    <Route path="/" component={App} />
+    <Route path="/" component={App} >
+        <Route path="greet" component={Greeting}/>
+        <Route path="greet2" component={Greeting}/>
+        <Route path="greet3" component={Greeting}/>
+    </Route>
 );
+
+/*
+
+/           App
+/greet      App, Greeeting
+/greet2     App, Greeeting
+/greet3     App, Greeeting
+
+*/
