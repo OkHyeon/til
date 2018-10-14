80270f7e8e34036b95c90e88a0a191c335b662c1
APPLE <>
2018-10-14 15:08:33 +0900

    ##78. 리액트 라우터의 인덱스 라우트

--- react/section6/simpleStart/src/routes.js
+++ react/section6/simpleStart/src/routes.js
@@ -2,25 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
-// ** 일반적으로 다른 라우터 파일을 이곳에 정의 하지 않는다.
 const Greeting = ()=>{
     return <div>hey there!</div>;
 }
 
 export default (
     <Route path="/" component={App} >
-        <Route path="greet" component={Greeting}/>
-        <Route path="greet2" component={Greeting}/>
-        <Route path="greet3" component={Greeting}/>
+        <IndexRoute component={PostsIndex} />
     </Route>
 );
-
-/*
-
-/           App
-/greet      App, Greeeting
-/greet2     App, Greeeting
-/greet3     App, Greeeting
-
-*/
